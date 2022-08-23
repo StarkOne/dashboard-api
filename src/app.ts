8b1524489bccd5d84f060a620d6e6b2a9ffcf281
StarkOne <stark.pro40@gmail.com>
2022-08-23 11:27:19 +0700

    init project with express and ts

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { router as usersRouter } from './users/users';
+
+export class App {
+  app: Express;
+  server: Server;
+  port: number;
+
+  constructor() {
+    this.app = express();
+    this.port = 8000;
+  }
+
+  useRoutes() {
+    this.app.use(usersRouter);
+  }
+
+  public async init() {
+    this.useRoutes();
+
+    this.server = this.app.listen(this.port, () => {
+      console.log(`Server start on port: ${this.port}`);
+    });
+  }
+}
