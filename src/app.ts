88a1212e384d25678a2e07b0dd02b4d7bb25292f
StarkOne <stark.pro40@gmail.com>
2022-08-23 12:24:27 +0700

    add base controller and logger

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,29 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
 import { router as usersRouter } from './users/users';
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
-    this.app.use(usersRouter);
+    this.app.use('/users', usersRouter);
   }
 
   public async init() {
     this.useRoutes();
 
     this.server = this.app.listen(this.port, () => {
-      console.log(`Server start on port: ${this.port}`);
+      this.logger.log(`Server start on port: ${this.port}`);
     });
   }
 }
